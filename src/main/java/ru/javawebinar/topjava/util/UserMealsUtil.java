d55fb2fd7040d632e31f523d7f583e6c6d3ed3ec
Ssssanja <61282557+Ssssanja@users.noreply.github.com>
2020-05-31 22:53:05 +0300

    Prepare to HW0 loops

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,12 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.time.chrono.ChronoLocalDateTime;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -28,8 +29,35 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+        List<UserMealWithExcess>result = new ArrayList<>();
+        Map<LocalDate, Integer> data = new TreeMap<>();
+        for (UserMeal uM : meals){
+            if (!data.containsKey(uM.getDateTime().toLocalDate())){
+                data.put(uM.getDateTime().toLocalDate(), uM.getCalories());
+            }
+            else {
+                int temporal = data.get(uM.getDateTime().toLocalDate());
+                temporal += uM.getCalories();
+                data.put(uM.getDateTime().toLocalDate(), temporal);
+            }
+        }
+        for (Map.Entry<LocalDate, Integer> entry : data.entrySet()){
+            if (entry.getValue()<=2000){
+                for (UserMeal uM : meals){
+                    if (uM.getDateTime().toLocalDate().isEqual(entry.getKey())&&uM.getDateTime().toLocalTime().isAfter(startTime)&&uM.getDateTime().toLocalTime().isBefore(endTime)){
+                        result.add(new UserMealWithExcess(uM.getDateTime(), uM.getDescription(), uM.getCalories(), false));
+                    }
+                }
+            }
+            else if (entry.getValue()>2000){
+                for (UserMeal uM : meals){
+                    if (uM.getDateTime().toLocalDate().isEqual(entry.getKey())&&uM.getDateTime().toLocalTime().isAfter(startTime)&&uM.getDateTime().toLocalTime().isBefore(endTime)){
+                        result.add(new UserMealWithExcess(uM.getDateTime(), uM.getDescription(), uM.getCalories(), true));
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
